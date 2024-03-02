2129d47046efdacac89465399f85beba4fc87284
Matthew Daniel Pareja <78730-22245567@SERVER2.DOMAIN>
2024-03-02 13:28:54 +0800

    Pareja Secondd Commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer (){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()}2024 IT-SYSARCH32-78730 Made by Matthew Daniel Pareja</p>
+        </footer>
+    )
+}
+
+export default Footer
